e188c00154f269be236a4d543571cf2455094a62
Vitalijs Pankovs <vitalijs.pankovs@gmail.com>
2023-12-08 01:10:21 +0200

    Added name validation.

--- src/modules/sensor/dto/sensor.dto.ts
+++ src/modules/sensor/dto/sensor.dto.ts
@@ -1,7 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { IsString } from 'class-validator'
+import { IsString, MaxLength } from 'class-validator'
 
 export class SensorDTO {
+  @MaxLength(25)
   @IsString()
   @ApiProperty()
   name: string
