ad19e779cb65400cfd548a3a7bb9f9a0a1c62cca
Vitalijs Pankovs <vitalijs.pankovs@gmail.com>
2024-01-06 15:35:11 +0200

    Test

--- src/modules/http/http.service.ts
+++ src/modules/http/http.service.ts
@@ -4,9 +4,9 @@ import { Options, mande } from 'mande'
 @Global()
 @Injectable()
 export class HttpService {
-  public async post<T>(url: string, data?: any, options?: Options<"json">) {
+  public async post<T>(url: string, data?: any) {
     try {
-      const res = await mande(url).post<T>(data, options)
+      const res = await mande(url).post<T>(data)
       console.log("RESULT = ", res)
       return res
     } catch (error) {
